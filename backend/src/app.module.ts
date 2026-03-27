d08ec1d6676e4386a05c0e5867a33e85084120f0
Toni <berendin@gmail.com>
2026-03-27 21:36:25 +0200

    add dotenv and env example

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -10,10 +10,10 @@ import { TaskModule } from './task/task.module';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '',
-      database: 'postgres',
+      port: Number(process.env.DB_PORT) ?? 5432,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [Task],
       synchronize: true, // disable for prod
       logging: true,
