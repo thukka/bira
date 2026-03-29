fbf2117e90249b7ea6bfa3f31d4689360d7d9dc9
Toni <berendin@gmail.com>
2026-03-29 14:25:33 +0300

    create data-source that can be used for migrations etc

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,23 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Task } from './task/task.entity';
 import { TaskModule } from './task/task.module';
 import { UserModule } from './user/user.module';
-import { User } from './user/user.entity';
 import { AuthModule } from './auth/auth.module';
+import { AppDataSource } from './data-source';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: Number(process.env.DB_PORT) ?? 5432,
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      entities: [Task, User],
-      synchronize: true, // disable for prod
-      logging: true,
+      ...AppDataSource.options,
     }),
     TaskModule,
     UserModule,
---
+++ backend/src/data-source.ts
@@ -0,0 +1,15 @@
+import { DataSource } from 'typeorm';
+import { Task } from './task/task.entity';
+import { User } from './user/user.entity';
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: 'localhost',
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [Task, User],
+  synchronize: false, // disable for prod
+  logging: true,
+});
