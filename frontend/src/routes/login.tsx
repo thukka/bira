1c7f55f52612d9620e2de787f440d17ede913943
Toni <berendin@gmail.com>
2026-04-02 23:12:34 +0300

    wip: stylings and labels

--- frontend/src/routes/login.tsx
+++ frontend/src/routes/login.tsx
@@ -2,9 +2,12 @@ import { Input } from "../components/input.tsx";
 
 const LoginPage = () => {
   return (
-    <form className="flex flex-col items-center gap-3.5">
-      <Input type="text" placeholder="username" />
-      <Input type="password" placeholder="password" />  
+    <form className="flex flex-col items-center justify-center h-screen gap-3.5">
+      <label htmlFor="username">Username:</label>
+      <Input type="text" id="username" placeholder="username" />
+      <label htmlFor="password">Password:</label>
+      <Input type="password" id="password" placeholder="password" />
+      <Input className="bg-lime-700 border-none font-black hover:bg-lime-600" type="submit" value="Log in" />
     </form>
   );
 };
