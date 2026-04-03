cf7ba2910f07daca73107aec55279ee47222df62
Toni <berendin@gmail.com>
2026-04-03 09:19:53 +0300

    wip: adjust style

--- frontend/src/components/input.tsx
+++ frontend/src/components/input.tsx
@@ -3,5 +3,5 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
 }
 
 export const Input = ({ className, ...props }: InputProps) => {
-  return <input className={`border rounded-md border-white h-10 w-[320px] ${className ?? ''}`} {...props} />;
+  return <input className={`border rounded-md border-white h-10 w-[320px] p-2 ${className ?? ''}`} {...props} />;
 };
--- frontend/src/routes/login.tsx
+++ frontend/src/routes/login.tsx
@@ -3,11 +3,23 @@ import { Input } from "../components/input.tsx";
 const LoginPage = () => {
   return (
     <form className="flex flex-col items-center justify-center h-screen gap-3.5">
-      <label htmlFor="username">Username:</label>
-      <Input type="text" id="username" placeholder="username" />
-      <label htmlFor="password">Password:</label>
-      <Input type="password" id="password" placeholder="password" />
-      <Input className="bg-lime-700 border-none font-black hover:bg-lime-600" type="submit" value="Log in" />
+      <div className="flex flex-col">
+        <label htmlFor="username" className="self-start mb-2">
+          Username:
+        </label>
+        <Input type="text" id="username" placeholder="username" />
+      </div>
+      <div className="flex flex-col">
+        <label htmlFor="password" className="self-start mb-2">
+          Password:
+        </label>
+        <Input type="password" id="password" placeholder="password" />
+      </div>
+      <Input
+        className="bg-lime-700 border-none font-black hover:bg-lime-600"
+        type="submit"
+        value="Log in"
+      />
     </form>
   );
 };
