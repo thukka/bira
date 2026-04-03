70b3f568a2807dd91540531ca6eab676bb4467b0
Toni <berendin@gmail.com>
2026-04-03 23:33:58 +0300

    add login fetch

--- frontend/src/components/login-form.tsx
+++ frontend/src/components/login-form.tsx
@@ -1,25 +1,58 @@
+import { useState, type ChangeEvent } from "react";
 import { Input } from "./input";
 
 export const LoginForm = () => {
+  const [user, setUser] = useState<string>();
+  const [password, setPassword] = useState<string>();
+
+  const handleLogin = async (e: ChangeEvent) => {
+    e.preventDefault();
+
+    console.log("user: ", user, "pw: ", password);
+
+    await fetch("/auth/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        username: user,
+        password: password,
+      }),
+    });
+  };
+
   return (
-    <form className="flex flex-col items-center justify-center h-screen gap-3.5">
+    <form
+      className="flex flex-col items-center justify-center h-screen gap-3.5"
+      onSubmit={handleLogin}
+    >
       <div className="flex flex-col">
         <label htmlFor="username" className="self-start mb-2">
           Username:
         </label>
-        <Input type="text" id="username" placeholder="username" />
+        <Input
+          type="text"
+          id="username"
+          placeholder="username"
+          onChange={(e) => setUser(e.target.value)}
+        />
       </div>
       <div className="flex flex-col">
         <label htmlFor="password" className="self-start mb-2">
           Password:
         </label>
-        <Input type="password" id="password" placeholder="password" />
+        <Input
+          type="password"
+          id="password"
+          placeholder="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </div>
-      <Input
-        className="bg-lime-700 border-none font-black hover:bg-lime-600"
+      <button
+        className="border rounded-md border-white h-10 w-[320px] p-2 bg-lime-700 border-none font-black hover:bg-lime-600"
         type="submit"
-        value="Log in"
-      />
+      >
+        Log in
+      </button>
     </form>
   );
 };
