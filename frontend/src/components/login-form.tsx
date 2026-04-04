0e3391b5cd28ed53d0b12824dd582ab193cfdb00
Toni <berendin@gmail.com>
2026-04-04 10:58:43 +0300

    show error messages to user

--- frontend/src/components/login-form.tsx
+++ frontend/src/components/login-form.tsx
@@ -1,58 +1,58 @@
 import { useState, type ChangeEvent } from "react";
 import { Input } from "./input";
 
-export const LoginForm = () => {
-  const [user, setUser] = useState<string>();
-  const [password, setPassword] = useState<string>();
+type Props = {
+  onSubmit: (user: string, password: string) => Promise<void>;
+  error?: string | null;
+};
 
-  const handleLogin = async (e: ChangeEvent) => {
+export const LoginForm = ({ onSubmit, error }: Props) => {
+  const [user, setUser] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  console.log("error", error);
+
+  const submitLogin = async (e: ChangeEvent) => {
     e.preventDefault();
 
-    console.log("user: ", user, "pw: ", password);
-
-    await fetch("/auth/login", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        username: user,
-        password: password,
-      }),
-    });
+    await onSubmit(user, password);
   };
 
   return (
-    <form
-      className="flex flex-col items-center justify-center h-screen gap-3.5"
-      onSubmit={handleLogin}
-    >
-      <div className="flex flex-col">
-        <label htmlFor="username" className="self-start mb-2">
-          Username:
-        </label>
-        <Input
-          type="text"
-          id="username"
-          placeholder="username"
-          onChange={(e) => setUser(e.target.value)}
-        />
-      </div>
-      <div className="flex flex-col">
-        <label htmlFor="password" className="self-start mb-2">
-          Password:
-        </label>
-        <Input
-          type="password"
-          id="password"
-          placeholder="password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
-      <button
-        className="border rounded-md border-white h-10 w-[320px] p-2 bg-lime-700 border-none font-black hover:bg-lime-600"
-        type="submit"
+    <>
+      <form
+        className="flex flex-col items-center justify-center h-screen gap-3.5"
+        onSubmit={submitLogin}
       >
-        Log in
-      </button>
-    </form>
+        <div className="flex flex-col">
+          <label htmlFor="username" className="self-start mb-2">
+            Username:
+          </label>
+          <Input
+            type="text"
+            id="username"
+            placeholder="username"
+            onChange={(e) => setUser(e.target.value)}
+          />
+        </div>
+        <div className="flex flex-col">
+          <label htmlFor="password" className="self-start mb-2">
+            Password:
+          </label>
+          <Input
+            type="password"
+            id="password"
+            placeholder="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button
+          className="border rounded-md border-white h-10 w-[320px] p-2 bg-lime-700 border-none font-black hover:bg-lime-600"
+          type="submit"
+        >
+          Log in
+        </button>
+        {error ? `Error: ${error}` : null}
+      </form>
+    </>
   );
 };
