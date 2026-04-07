0e0c7dcfbeac93dea04e38d9adfe5654ff4a2647
Toni <berendin@gmail.com>
2026-04-07 20:30:01 +0300

    WIP: testing task fetch

--- frontend/src/routes/task.tsx
+++ frontend/src/routes/task.tsx
@@ -1,3 +1,42 @@
+import { useEffect, useState } from "react";
+import type { Task } from "../../../interface/task.interface";
+
 export const TaskPage = () => {
-    return <div>task page</div>;
-}
+  const [tasks, setTasks] = useState<Task[] | null>(null);
+
+  useEffect(() => {
+    const getTasks = async (): Promise<Task[]> => {
+      try {
+        const res = await fetch("/api/task", {
+          method: "GET",
+          headers: { "Content-Type": "application/json" },
+        });
+
+        if (!res.ok) {
+          throw new Error("Response status: " + res.status);
+        }
+
+        const data = await res.json();
+        setTasks(data);
+      } catch (error) {
+        console.log(error);
+      }
+
+      return [];
+    };
+
+    getTasks();
+  }, []);
+
+  console.log("tasks: ", tasks);
+
+  return (
+    <div>
+      {tasks?.map((x) => (
+        <p>
+          {x.assigned} {x.subject}<br />
+        </p>
+      ))}
+    </div>
+  );
+};
